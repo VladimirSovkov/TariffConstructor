0639d3fdf127f86814c451b81836fd4eaa0b65c6
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-04 16:06:12 +0300

    Add ProductOptionTariffPriceMap

--- src/TariffConstructor.Infrastructure/Data/TariffConstructorContext.cs
+++ src/TariffConstructor.Infrastructure/Data/TariffConstructorContext.cs
@@ -1,11 +1,14 @@
-﻿
-using Microsoft.EntityFrameworkCore;
-using TariffConstructor.Domain.ObjectTests;
+﻿using Microsoft.EntityFrameworkCore;
 using TariffConstructor.Domain.ProductAggregate;
+using TariffConstructor.Domain.ProductOptionAggregate;
+using TariffConstructor.Domain.ProductOptionKindAggregate;
+using TariffConstructor.Domain.ProductOptionTariffAggregate;
 using TariffConstructor.Domain.TariffAggregate;
 using TariffConstructor.Infrastructure.Data.TariffConstructorModel.EntityConfigurations;
-using TariffConstructor.Infrastructure.Data.TariffConstructorModel.EntityConfigurations.ObjectTests.Mapping;
 using TariffConstructor.Infrastructure.Data.TariffConstructorModel.EntityConfigurations.ProductAggregateMap;
+using TariffConstructor.Infrastructure.Data.TariffConstructorModel.EntityConfigurations.ProductOptionAggregate.Mapping;
+using TariffConstructor.Infrastructure.Data.TariffConstructorModel.EntityConfigurations.ProductOptionKindAggregate.Mapping;
+using TariffConstructor.Infrastructure.Data.TariffConstructorModel.EntityConfigurations.ProductOptionTariffAggregate.Mapping;
 using TariffConstructor.Infrastructure.Data.TariffConstructorModel.EntityConfigurations.TariffAgregate.Mapping;
 
 namespace TariffConstructor.Infrastructure.Data
@@ -19,12 +22,22 @@ namespace TariffConstructor.Infrastructure.Data
 
         public DbSet<IncludedProductInTariff> IncludedProductInTariff { get; set; }
         public DbSet<Product> Products { get; set; }
-        //public DbSet<Tariff> Tariff { get; set; }
         //public DbSet<TariffTestPeriod> TariffTestPeriods { get; set; }
         //сделано для тестов работы разных вариантов Fluent Api
         //public DbSet<ClassWithEnum> classWithEnums { get; set; }
-        public DbSet<TariffPrice> TariffPrices {get; set;}
+        public DbSet<TariffPrice> TariffPrices { get; set; }
         public DbSet<Tariff> Tariffs { get; set; }
+
+        //ProductOptionAggregate
+        public DbSet<ProductOption> ProductOptions { get; set; }
+
+        //ProductOptionKindAggregate
+        public DbSet<ProductOptionKind> ProductOptionKinds { get; set; }
+
+        //ProductOptionTariffAggregate
+        public DbSet<ProductOptionTariffPrice> productOptionTariffPrices { get; set; }
+
+
 
         protected override void OnModelCreating(ModelBuilder builder )
         {
@@ -32,15 +45,30 @@ namespace TariffConstructor.Infrastructure.Data
             
             builder.HasSequence<int>(HiLoSequence.DBSequenceHiLoForTariffAdvancePrice).StartsAt(1)
                 .IncrementsBy(1);
+            //TariffAgregate
+            builder.ApplyConfiguration(new TariffMap());
+            builder.ApplyConfiguration(new TariffAdvancePriceMap());
+            builder.ApplyConfiguration(new TariffPriceMap());
+            builder.ApplyConfiguration(new IncludedProductInTariffMap());
+
+            //ProductAggregate
+            builder.ApplyConfiguration(new ProductMap());
+
+            //ProductOptionKindAggregate
+            builder.ApplyConfiguration(new ProductOptionKindMap());
+
+            //ProductOptionAggregate
+            builder.ApplyConfiguration(new ProductOptionMap());
+
+            //ProductOptionTariffAggregate
+            builder.ApplyConfiguration(new ProductOptionTariffPriceMap());
+            //builder.ApplyConfiguration(new ProductOptionTariffMap());
+
+
 
             //builder.Entity<IncludedProductInTariff>().ToTable("test", schema: "dbo");
-            builder.ApplyConfiguration( new IncludedProductInTariffMap() );
-            builder.ApplyConfiguration(new ProductMap());
-            builder.ApplyConfiguration(new TariffAdvancePriceMap());
-            builder.ApplyConfiguration(new TariffMap());
             //builder.ApplyConfiguration(new TariffTestPeriodMap());
             //builder.ApplyConfiguration(new ClassWithEnumMap());
-            builder.ApplyConfiguration(new TariffPriceMap());
         }
     }
 }
---
+++ src/TariffConstructor.Infrastructure/Data/TariffConstructorModel/EntityConfigurations/ProductOptionTariffAggregate/Mapping/ProductOptionTariffPriceMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TariffConstructor.Domain.ProductOptionTariffAggregate;
+
+namespace TariffConstructor.Infrastructure.Data.TariffConstructorModel.EntityConfigurations.ProductOptionTariffAggregate.Mapping
+{
+    public class ProductOptionTariffPriceMap : IEntityTypeConfiguration<ProductOptionTariffPrice>
+    {
+        public void Configure(EntityTypeBuilder<ProductOptionTariffPrice> builder)
+        {
+            builder.ToTable("ProductOptionTariffPrice");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseHiLo(HiLoSequence.DBSequenceHiLoForTariffAdvancePrice);
+            
+            builder.OwnsOne(x => x.Price, y => y.Property(p => p.Currency).HasMaxLength(3));
+            builder.OwnsOne(x => x.Period);
+
+            builder.HasOne(x => x.ProductOptionTariff).WithMany(x => x.Prices).HasForeignKey(x => x.ProductOptionTariffId);
+
+            builder.Property(x => x.CreationDate).ValueGeneratedOnAdd();
+        }
+    }
+}
