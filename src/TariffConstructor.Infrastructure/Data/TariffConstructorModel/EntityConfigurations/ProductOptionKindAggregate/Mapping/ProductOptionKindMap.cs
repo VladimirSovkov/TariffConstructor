0ae1966062ddaefe7baa06a0da255786c5308ed5
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-04 13:03:32 +0300

    Add ProductOptionKindMap

---
+++ src/TariffConstructor.Infrastructure/Data/TariffConstructorModel/EntityConfigurations/ProductOptionKindAggregate/Mapping/ProductOptionKindMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TariffConstructor.Domain.ProductOptionKindAggregate;
+
+namespace TariffConstructor.Infrastructure.Data.TariffConstructorModel.EntityConfigurations.ProductOptionKindAggregate.Mapping
+{
+    public class ProductOptionKindMap : IEntityTypeConfiguration<ProductOptionKind>
+    {
+        public void Configure(EntityTypeBuilder<ProductOptionKind> builder)
+        {
+            builder.ToTable("ProductOptionKind");
+
+            builder.HasKey(x => x.Id);
+            
+            builder.HasIndex(x => x.TenantId);
+
+            builder.Property(x => x.Name).HasMaxLength(255).IsRequired();
+            builder.Property(x => x.PublicId).HasMaxLength(100);
+
+            builder.Property(x => x.CreationDate).ValueGeneratedOnAdd();
+        }
+    }
+}
