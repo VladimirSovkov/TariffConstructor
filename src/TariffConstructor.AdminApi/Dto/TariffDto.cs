f1b9da8e94a05d90a5c70ff713b424c782f419ce
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-17 09:53:37 +0300

    Lj,fdbk TariffDto и мапер к нему

---
+++ src/TariffConstructor.AdminApi/Dto/TariffDto.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using TariffConstructor.Domain.TariffAggregate;
+
+namespace TariffConstructor.AdminApi.Dto
+{
+    [DataContract]
+    public class TariffDto
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "tenantId")]
+        public int TenantId { get; set; }
+
+        [DataMember(Name = "publicId")]
+        public string PublicId { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "paymentType")]
+        public int PaymentType { get; set; }
+
+        //[DataMember(Name = "contractKindBindings")]
+        //public List<TariffToContractKindBinding> ContractKindBindings { get; set; }
+    }
+}
---
+++ src/TariffConstructor.AdminApi/Mappers/TariffAggregate/TariffMap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TariffConstructor.AdminApi.Dto;
+using TariffConstructor.Domain.TariffAggregate;
+
+namespace TariffConstructor.AdminApi.Mappers.TariffAggregate
+{
+    public static class TariffMap
+    {
+        public static TariffDto Map(this Tariff tariff)
+        {
+            if (tariff == null)
+            {
+                return null;
+            }
+            else
+            {
+                return new TariffDto
+                {
+                    Id = tariff.Id,
+                    TenantId = tariff.TenantId,
+                    PublicId = tariff.PublicId,
+                    Name = tariff.Name,
+                    PaymentType = (int)tariff.PaymentType
+                    //ContractKindBindings = tariff.ContractKindBindings.ToList()
+                };
+            }
+        }
+
+        public static List<TariffDto> Map(this IEnumerable<Tariff> tariff)
+        {
+            return tariff == null ? new List<TariffDto>() : tariff.ToList().ConvertAll(Map);
+        }
+    }
+}
