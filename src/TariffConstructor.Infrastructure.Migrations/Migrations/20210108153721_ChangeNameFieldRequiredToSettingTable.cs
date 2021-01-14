8d8901db685e9be918f38db64681772f0e500c8a
VladimirSovkov <vladimir.sovkov@yandex.ru>
2021-01-14 21:31:09 +0300

    в бд (Setting) сделал Name обязательным

---
+++ src/TariffConstructor.Infrastructure.Migrations/Migrations/20210108153721_ChangeNameFieldRequiredToSettingTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TariffConstructor.Infrastructure.Migrations.Migrations
+{
+    public partial class ChangeNameFieldRequiredToSettingTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Setting",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Setting",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255);
+        }
+    }
+}
